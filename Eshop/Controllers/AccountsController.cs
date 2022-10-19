e6e855147b08ca848a5e7246a35b424d75c57570
lechitrung-cdpm20D <0306201394@caothang.edu.vn>
2022-10-19 15:21:52 +0700

    update

---
+++ Eshop/Controllers/AccountsController.cs
@@ -0,0 +1,80 @@
+﻿using Eshop.Data;
+using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshop.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private EshopContext _context;
+        public AccountsController (EshopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var accounts=_context.Accounts.ToList();
+            return View(accounts);
+        }
+        public IActionResult Details(int? id)
+        {
+            if(id== null)
+            {
+                return RedirectToAction("Index");
+            }
+            var accounts=_context.Accounts.FirstOrDefault(x=>x.Id==id);
+            if (accounts == null)
+            {
+                return NotFound();
+            }
+            return View(accounts);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var account = _context.Accounts.FirstOrDefault(x => x.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            _context.Accounts.Remove(account);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create([Bind("Username,Password,Email,Phone,Address,FullName,Avatar")] Account account)
+        {
+            _context.Accounts.Add(account);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var accounts = _context.Accounts.FirstOrDefault(x => x.Id == id);
+            if (accounts == null)
+            {
+                return NotFound();
+            }
+            return View(accounts);
+        }
+        [HttpPost]
+        public IActionResult Edit([Bind("Username,Password,Email,Phone,Address,FullName,Avatar")] Account account)
+        {
+            _context.Accounts.Update(account);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Eshop/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using Eshop.Data;
+using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private EshopContext _context;
+        public ProductsController (EshopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+        public IActionResult Details (int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var products = _context.Products.FirstOrDefault(x => x.Id == id);
+            if(products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+        public IActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create([Bind("Name,SKU,Description,Price,Image")] Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(int? id, [Bind("Id,Name,SKU,Description,Image,Price")] Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
